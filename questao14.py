4741a0511b99c3b3bea63c2cbaddb620fd9efcf3
Gabriel Ferreira <willamigabrielferreiradesouza@gmail.com>
2022-11-07 09:39:06 -0300

    Questões 13 e 14 não terminadas

---
+++ questao14.py
@@ -0,0 +1,17 @@
+'''Escreva uma função que recebes 3 valores reais X, Y e Z e que verifique se
+esses valores podem ser os comprimentos dos lados de um triângulo e, neste
+caso, retornar qual o tipo de triângulo formado. Para que X, Y e Z formem um
+triângulo é necessário que a seguinte propriedade seja satisfeita: o comprimento
+de cada lado de um triângulo é menor do que a soma do comprimento dos outros
+dois lados. O procedimento deve identificar o tipo de triângulo formado
+observando as seguintes definições:
+
+o Triângulo Equilátero: os comprimentos dos 3 lados são iguais.
+o Triângulo Isósceles: os comprimentos de 2 lados são iguais.
+o Triângulo Escaleno: os comprimentos dos 3 lados são diferentes.'''
+
+'''
+1 - Criar a função que recebe os tres lados como parâmetro. Essa função verifica se
+com os valores é possível fazer um triângulo ou não. Se for possível, a função deve
+classificar o triângulo.
+'''
