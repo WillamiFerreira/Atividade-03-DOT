0d9d6d9758377e48b10288e79e336f7ec94e79f6
Gabriel Ferreira <willamigabrielferreiradesouza@gmail.com>
2022-11-28 08:45:53 -0300

    questao24 concluida

---
+++ questao24.py
@@ -0,0 +1,26 @@
+'''Escreva uma função que recebe, por parâmetro, dois valores X e Z e calcula
+e retorna Xz. (sem utilizar funções ou operadores de potência prontos)'''
+def verificardor(x, y):
+    if type(x) == int or type(x) == float:
+        if type(y) != int:
+            return False
+        else: return True
+    else:
+        return False
+
+def potenciacao(x, y):
+    if verificardor(x, y):
+        resultado = 1
+        for _ in range(y):
+            resultado *= x
+        print(resultado)
+        return round(resultado, 2)
+    else:
+        return Exception
+
+assert potenciacao(2, 2) == 4
+assert potenciacao(10, 2) == 100
+assert potenciacao(21.4, 2) == 457.96
+assert potenciacao(2, 2.3) == Exception
+assert potenciacao('string', 'string') == Exception
+print('Todos os testes estão OK')
