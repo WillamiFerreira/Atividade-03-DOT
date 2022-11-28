63c419eadb8ac4d8434c81ef9596d16929fdce41
Gabriel Ferreira <willamigabrielferreiradesouza@gmail.com>
2022-11-28 08:03:55 -0300

    função da questão dois renomeada e questão 22 concluida

--- questao02.py
+++ questao02.py
@@ -3,7 +3,7 @@ letra. Se a letra for A o procedimento calcula a média aritmética das notas do
 aluno, se for P, a sua média ponderada (pesos: 5, 3 e 2). A função deve retornar
 a média calculada."""
 
-def Calcula_Media(n1, n2, n3, letra):
+def calcula_media(n1, n2, n3, letra):
     if letra in "Aa" :
         media = round(((n1 + n2 + n3) / 3), 2)
         return media
@@ -14,10 +14,10 @@ def Calcula_Media(n1, n2, n3, letra):
         return Exception
 
 try:
-    assert Calcula_Media(8.5, 9, 10, "A" ) == 9.17
-    assert Calcula_Media(6.7, 10, 2, "P") == 6.75
-    assert Calcula_Media(5, 6.7, 8.9, "z") == Exception
-   #assert Calcula_Media(4, 5, "A") == Exception
+    assert calcula_media(8.5, 9, 10, "A" ) == 9.17
+    assert calcula_media(6.7, 10, 2, "P") == 6.75
+    assert calcula_media(5, 6.7, 8.9, "z") == Exception
+   #assert calcula_media(4, 5, "A") == Exception
 except TypeError:
     print('Faltando notas ou tipo de operação.')
 else:
---
+++ questao22.py
@@ -0,0 +1,22 @@
+from math import factorial
+'''Escreva uma função que recebe por parâmetro um valor inteiro e positivo N
+e retorna o valor de S.
+S = 1 + 1/1! + 1⁄2! + 1/3! + 1 /N!'''
+
+def calcula_valor_s(n):
+    if type(n) == int and n > 0:
+        resultado = 0
+        for v in range(0, n+1):
+            fac = factorial(v)
+            resultado += 1/fac
+        print(round(resultado, 2))
+        return round(resultado, 2)
+    else:
+        return Exception
+
+assert calcula_valor_s(5) == 2.72
+assert calcula_valor_s('t') == Exception
+assert calcula_valor_s(4.33) == Exception
+assert calcula_valor_s(-34) == Exception
+print('todos dos testes estão OK')
+
